65169db85dc2bb5b67271538ed95e2d4ed5b7759
ChrisReilly024 <chris.reilly024@gmail.com>
2021-07-21 15:10:32 -0400

    tab section complete

--- src/components/header.js
+++ src/components/header.js
@@ -16,6 +16,7 @@ const Header = (title, date, temp) => {
     const titleTag = document.createElement('h1');
     const tempTag = document.createElement('span')
 
+    header.className = 'header'
     dateTag.classList.add('date');
     tempTag.classList.add('temp');
 
@@ -27,13 +28,13 @@ const Header = (title, date, temp) => {
     return header;
 }
 
-const headerAppender = (selector) => { document.querySelector(selector).appendChild(Header('7/20/2022', 'Heatwave!','96F'))
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
-}
+// TASK 2
+// ---------------------
+// Implement this function taking a css selector as its only argument.
+// It should create a header using the Header component above, passing arguments of your choosing.
+// It should append the header to the element in the DOM that matches the given selector.
+//
+const headerAppender = (selector) => document.querySelector(selector).append(Header('Lambda Times','7/20/2022', '96F'))
 
-export { Header, headerAppender }
+
+export {Header, headerAppender}
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,4 +1,21 @@
-const Tabs = (topics) => {
+import axios from 'axios';
+
+
+const Tabs = topics => {
+
+  const div = document.createElement('div');
+  div.className = 'topics';
+
+  topics.forEach(topic => {
+    const subDiv = document.createElement('div')
+    subDiv.className = 'tab';
+    subDiv.textContent = topic;
+    div.appendChild(subDiv);
+  })
+  
+  return div
+}
+
   // TASK 3
   // ---------------------
   // Implement this function which takes an array of strings ("topics") as its only argument.
@@ -12,17 +29,22 @@ const Tabs = (topics) => {
   //   <div class="tab">bootstrap</div>
   //   <div class="tab">technology</div>
   // </div>
-  //
-}
+  
 
-const tabsAppender = (selector) => {
+
+const tabsAppender = selector => {
+  axios.get('http://localhost:5000/api/topics')
+  .then(response => document.querySelector(selector).appendChild(Tabs(response.data.topics)))
+  .catch(error => error);
+}
+  
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
   // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
-}
+  
+
 
 export { Tabs, tabsAppender }
